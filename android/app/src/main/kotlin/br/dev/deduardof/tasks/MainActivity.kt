0a791481e116c2f46b4e9211508992ac46f6478e
Douglas Eduardo <deduardof@gmail.com>
2025-06-08 10:41:24 -0300

    Created project structure

---
+++ android/app/src/main/kotlin/br/dev/deduardof/tasks/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.dev.deduardof.tasks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:tasks/tasks_app.dart';
+
+void main() {
+  runApp(const TasksApp());
+}
